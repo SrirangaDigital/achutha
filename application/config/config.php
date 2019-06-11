b717c1a7763e37834223bded10c2a855bcd725e1
Suresh <soori_rozee@yahoo.com>
2019-06-11 13:53:38 +0530

    Config folder modification

--- application/config/config.php
+++ application/config/config.php
@@ -1,19 +1,12 @@
 <?php
 
-	define('BASE_URL', 'http://localhost/udayabhanu/');
-	define('AUTHENTICATION_URL', 'http://localhost/auth-udayabhanu/');
+	define('BASE_URL', 'http://localhost/achuta/');
 	define('PUBLIC_URL', BASE_URL . 'public/');
-	define('DATA_URL', PUBLIC_URL . 'pdf/');
-	define('XML_SRC_URL', BASE_URL . 'md-src/xml/');
-	define('PHOTO_URL', PUBLIC_URL . 'Photos/');
-	define('DOWNLOAD_URL', PUBLIC_URL . 'Downloads/');
 	define('IMAGE_URL', PUBLIC_URL . 'images/');
-	define('RESOURCES_URL', PUBLIC_URL . 'Resources/');
-
+	
 	// Physical location of resources
-	define('PHY_BASE_URL', '/var/www/html/udayabhanu/');
+	define('PHY_BASE_URL', '/var/www/html/achuta/');
 	define('PHY_PUBLIC_URL', PHY_BASE_URL . 'public/');
-	define('PHY_XML_SRC_URL', PHY_BASE_URL . 'md-src/xml/');
 	define('PHY_FLAT_URL', PHY_BASE_URL . 'application/views/flat/');
 	define('PHY_IMAGE_URL', PHY_PUBLIC_URL . 'images/');
 
